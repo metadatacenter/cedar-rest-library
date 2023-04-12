ba8c8acb6d1ee494b42f1df15318db72e57772f8
Attila L. Egyedi <egyedia@stanford.edu>
2023-04-11 23:38:03 -0700

    Initial commit

---
+++ src/main/java/org/metadatacenter/constant/CedarQueryParameters.java
@@ -0,0 +1,28 @@
+package org.metadatacenter.constant;
+
+public final class CedarQueryParameters {
+
+  private CedarQueryParameters() {
+  }
+
+  public static final String QP_FOLDER_ID = "folder_id";
+  public static final String QP_DEBUG = "debug";
+  public static final String QP_RESOURCE_TYPES = "resource_types";
+  public static final String QP_RESOURCE_TYPE = "resource_type";
+  public static final String QP_SORT = "sort";
+  public static final String QP_LIMIT = "limit";
+  public static final String QP_OFFSET = "offset";
+  public static final String QP_VERSION = "version";
+  public static final String QP_PUBLICATION_STATUS = "publication_status";
+  public static final String QP_SUMMARY = "summary";
+  public static final String QP_FIELD_NAMES = "field_names";
+  public static final String QP_IS_BASED_ON = "is_based_on";
+  public static final String QP_Q = "q";
+  public static final String QP_ID = "id";
+  public static final String QP_SHARING = "sharing";
+  public static final String QP_MODE = "mode";
+  public static final String QP_FORMAT = "format";
+  public static final String QP_NOTIFICATION_STATUS = "notification_status";
+  public static final String QP_CATEGORY_ID = "category_id";
+
+}
